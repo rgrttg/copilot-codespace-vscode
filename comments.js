c43d2a22289c2e6b214b3b89ef3d5304e3397fcf
Roger <151507205+rgrttg@users.noreply.github.com>
2024-05-29 12:19:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,56 @@
 // create web server
-// npm install express
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// npm install cors
-var cors = require('cors');
-app.use(cors());
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
 
-// npm install mongoose
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/comments');
+// Use the body-parser middleware to parse the body of the request
+app.use(bodyParser.json())
 
-var commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String
-});
+// Path: /comments
+app.get('/comments', (req, res) => {
+    // read the comments.json file
+    const comments = fs.readFileSync(path.join(__dirname, 'comments.json'), 'utf8')
+    // send the comments as a response
+    res.send(comments)
+})
 
-var Comment = mongoose.model('Comment', commentSchema);
+// Path: /comments
+app.post('/comments', (req, res) => {
+    // read the comments.json file
+    const comments = fs.readFileSync(path.join(__dirname, 'comments.json'), 'utf8')
+    // parse the comments to an array
+    const commentsArray = JSON.parse(comments)
+    // add a new comment to the array
+    commentsArray.push({
+        id: uuidv4(),
+        name: req.body.name,
+        comment: req.body.comment
+    })
+    // write the updated comments to the comments.json file
+    fs.writeFileSync(path.join(__dirname, 'comments.json'), JSON.stringify(commentsArray))
+    // send a response
+    res.send('Comment added!')
+})
 
-// create a comment
-app.post('/comments', function (req, res) {
-    var comment = new Comment(req.body);
-    comment.save(function (err) {
-        if (err)
-            res.send(err);
-        res.json(req.body);
-    });
-});
+// Path: /comments
+app.delete('/comments/:id', (req, res) => {
+    // read the comments.json file
+    const comments = fs.readFileSync(path.join(__dirname, 'comments.json'), 'utf8')
+    // parse the comments to an array
+    const commentsArray = JSON.parse(comments)
+    // find the index of the comment with the id from the URL
+    const commentIndex = commentsArray.findIndex(comment => comment.id === req.params.id)
+    // remove the comment from the array
+    commentsArray.splice(commentIndex, 1)
+    // write the updated comments to the comments.json file
+    fs.writeFileSync(path.join(__dirname, 'comments.json'), JSON.stringify(commentsArray))
+    // send a response
+    res.send('Comment deleted!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
